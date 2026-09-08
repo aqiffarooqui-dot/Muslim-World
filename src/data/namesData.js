1a0f9d1139fcc96cc78a84a796b88952d8264868
aqiffarooqui-dot <aqiffarooqui@gmail.com>
2026-09-09 01:32:40 +0530

    Create namesData.js

---
+++ src/data/namesData.js
@@ -0,0 +1,10 @@
+export const namesList = [
+  { no: 1, arabic: "ٱلرَّحْمَٰنُ", name: "Ar-Rahman", meaning: "The Beneficent" },
+  { no: 2, arabic: "ٱلرَّحِيمُ", name: "Ar-Rahim", meaning: "The Merciful" },
+  { no: 3, arabic: "ٱلْمَلِكُ", name: "Al-Malik", meaning: "The King / Sovereign" },
+  { no: 4, arabic: "ٱلْقُدُّوسُ", name: "Al-Quddus", meaning: "The Pure / Holy" },
+  { no: 5, arabic: "ٱلسَّلَامُ", name: "As-Salam", meaning: "The Source of Peace" },
+  { no: 6, arabic: "ٱلْمُؤْمِنُ", name: "ٱلْمُؤْمِنُ", meaning: "The Guardian of Faith" },
+  { no: 7, arabic: "ٱلْمُهَيْمِنُ", name: "Al-Muhaymin", meaning: "The Protector / Overseer" },
+  { no: 8, arabic: "ٱلْعَزِيزُ", name: "Al-Aziz", meaning: "The Almighty / Mighty" }
+];
